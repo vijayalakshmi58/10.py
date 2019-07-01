7f7913bd70c8105c479167fed4b8a85d97a773ce
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-01 12:09:21 +0530

    Update and rename README.md to 10.py

---
+++ 10.py
@@ -0,0 +1,8 @@
+# 10.py
+Number = int(input("Please Enter any Number: "))
+Count = 0
+while(Number > 0):
+    Number = Number // 10
+    Count = Count + 1
+
+print("\n Number of Digits in a Given Number = %d" %Count)
